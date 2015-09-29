455298154632d5200576f3296e45cdbce1c8fe35
Romario Coffie <Romario.agc@gmail.com>
2015-09-29 01:14:00 -0400

    Worked in Index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mongoose = require('mongoos');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var _=require('lodash');
@@ -12,8 +12,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+//CORS Support
+app.use(function(req,res,next){
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+app.use('/hello', function(req, res, next){
+	res.send('Hello World!');
+	next();
+});
+
 //Connect to MongoDB
-mongoos.connect('mongodb://localhost/darwinapp');
+mongoose.connect('mongodb://localhost/darwinapp');
 mongoose.connection.once('open',function(){
 
 console.log('Listening on port 3000...');
