d679c3b3af772aeda4ddabe46e4fcc80711962f2
Romario Coffie <Romario.agc@gmail.com>
2015-09-28 20:19:06 -0400

    Renamed a few stuff and played around with the JSON files. Nothing serious yet.

--- frontpage.js
+++ frontpage.js
@@ -20,27 +20,20 @@ function download(url, callback) {
 }
 // Source url
 var url = "https://www.reddit.com/r/leagueoflegends"
-
+var update = [] ;
 
 download(url, function(data) {
   if (data) {
 
-/*
-    var postrank: = { rank1: "", rank2: "", rank3: "", rank4: "", rank5: "", rank6: "", rank7: "", rank8: ""
-      rank9: "", rank10: "", rank11: "", rank12: "", rank13: "", rank14: "", rank15: "", rank16: ""
-      rank17: "", rank18: "", rank19: "", rank20: "", rank21: "", rank22: "", rank23: "", rank24: ""
-      rank25: ""
-    }
-*/
-    var update = " ";
     var $ = cheerio.load(data);
     // Parse's url for specified data
     $("a.title.may-blank ").each(function(i, e) {
-      update += $(e).html();
+      update.push($(e).html());
       console.log($(e).html());
       });
     // Writes data to file
     fs.writeFileSync('update.json', util.inspect(update) , 'utf-8'); 
+    fs.writeFileSync('update.txt', util.inspect(update) , 'utf-8');
 
 
     console.log("Finished update sir, a file copy 'input.json' was made and stored in the directory ");
