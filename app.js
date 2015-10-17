1021e66a0d5a9969936db826dfc84915223a6ef7
Romario Coffie <Romario.agc@gmail.com>
2015-10-17 19:43:30 -0400

    Got mongoDB to work and set up parse to work in router.
    Changed Database to ‘funnel’
    
    Prints JSON in collection in webpage then empties collection for next
    GET request for parser

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 //Depencencies
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var _ = require('lodash');
-
-//Create the application
-var app = express();
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    _ = require('lodash'),
+    app = express();
 
 // Add Middleware necessarry for REST API's
 app.use(bodyParser.urlencoded({extended: true}));
@@ -23,99 +21,29 @@ app.use(function(req, res, next) {
 
 
 //Routes
-app.use('/dashboard', function(req, res, next) {
-  res.send('/r/leagueoflegends');
+//app.use('*', function(req, res, next) {
+  //res.send('404 Page not found');
+  //next();
+//});
+
+app.use('/home', function(req, res, next) {
+  res.send('Welcome to funnel');
   next();
 });
 
-app.use('/settings', function(req, res, next) {
-  res.send('Settings');
-  next();
-});
-
-app.use('/', require('./routes/index'));
+app.use('/posts', require('./routes/index'));
 
 
 //Connect to MongoDB
-mongoose.connect('mongodb://localhost/darwinapp');
-mongoose.connection.once('open', function() {
-
-  /*
-  //request for single post data
-   app.get('/api/post1', function(req, res) {
-
-          // use mongoose to get all posts in the database
-          post.find(function(err, post) {
-
-              // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-              if (err)
-                  res.send(err)
-
-              res.json(post); // return all single posts in JSON format
-          });
-      });
-
-
-      // get all todos
-      app.get('/api/todos', function(req, res) {
-
-          // use mongoose to get all todos in the database
-          Todo.find(function(err, todos) {
-
-              // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-              if (err)
-                  res.send(err)
-
-              res.json(todos); // return all todos in JSON format
-          });
-      });
-
-      // create todo and send back all todos after creation
-      app.post('/api/todos', function(req, res) {
-
-          // create a todo, information comes from AJAX request from Angular
-          Todo.create({
-              text : req.body.text,
-              done : false
-          }, function(err, todo) {
-              if (err)
-                  res.send(err);
-
-              // get and return all the todos after you create another
-              Todo.find(function(err, todos) {
-                  if (err)
-                      res.send(err)
-                  res.json(todos);
-              });
-          });
-
-      });
-
-      // delete a todo
-      app.delete('/api/todos/:todo_id', function(req, res) {
-          Todo.remove({
-              _id : req.params.todo_id
-          }, function(err, todo) {
-              if (err)
-                  res.send(err);
-
-              // get and return all the todos after you create another
-              Todo.find(function(err, todos) {
-                  if (err)
-                      res.send(err)
-                  res.json(todos);
-              });
-          });
-      });
-
-
+mongoose.connect('mongodb://localhost/funnel');
+mongoose.connection.once('open', function(err) {
+  if (err) throw err;
+/*
    // application -------------------------------------------------------------
       app.get('*', function(req, res) {
           res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
       });
   */
-
-
   console.log('Listening on port 3000...');
   app.listen(3000);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 //Dependencies
-var https = require("https");
-var cheerio = require("cheerio");
-var mongoose = require('mongoose');
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
-var util = require('util');
+var https = require("https"),
+    cheerio = require("cheerio"),
+    mongoose = require('mongoose'),
+    express = require('express'),
+    router = express.Router(),
+    fs = require('fs'),
+    util = require('util');
 
 
 //  Utility function that downloads a URL and invokes callback with the data.
@@ -24,73 +24,58 @@ function download(url, callback) {
 }
 
 
-//Conneting to MongoDB
-var db = mongoose.connection;
-
 // Source url
 var url = "https://www.reddit.com/r/leagueoflegends";
 
+
 //Importing Schemas
-var post = require('../models/post');
-var updatelist = require('../models/updatelist');
-var r = 11;
+var post = require('../models/post'),
+  updatelist = require('../models/updatelist');
 
 
 //Routes
-post.methods(['get', 'put', 'post', 'delete']);
-post.register(router, '/');
+router.get('/history', function(req, res){
+  //Pulling data from url
+  download(url, function(data) {
+    if (data) {
 
-updatelist.methods(['get', 'put', 'post', 'delete']);
-updatelist.register(router, '/updatelist');
+      //Loads html into cheerio
+      var $ = cheerio.load(data);
 
-router.get('/history', function(req, res) {
-  //Response to request with post database
-  post.find({},function(err,post){
-    if (err)
-    res.send(err);
-    res.json(post);
-  });
-});
-
-
-//Pulling data from url
-download(url, function(data) {
-  if (data) {
-    //Loads html into cheerio
-    var $ = cheerio.load(data);
-
-    // Parse's html in loop for specified data
-    $("a.title.may-blank").each(function(i, e) {
-      //Save data to model as an individual post
-      var singlepost = new post({
-        rank: $("span.rank").eq(i).text(),
-        score: $("div.score.unvoted").eq(i).text(),
-        title: $(e).html(),
-        link: $("a.thumbnail.self.may-blank").attr('href'),
-        time: $("time.live-timestamp").eq(i).text(),
-        comments: $("a.comments.may-blank").eq(i).text()
+      // Parse's html in loop for specified data
+      $("a.title.may-blank").each(function(i, e) {
+        //Save data to model as an individual post
+        var singlepost = new post({
+          rank: $("span.rank").eq(i).text(),
+          score: $("div.score.unvoted").eq(i).text(),
+          title: $(e).html(),
+          link: $("a.thumbnail.self.may-blank").attr('href'),
+          time: $("time.live-timestamp").eq(i).text(),
+          comments: $("a.comments.may-blank").eq(i).text()
+        });
+        console.log(singlepost);
+        singlepost.save();
       });
-      console.log(singlepost);
-
-      //Save model to Database and saving ID
-      singlepost.save(function (err) {
-        if (err) return handleError(err);
-        // saved!
-        //spid = singlepost.id;
-        //console.log(spid);
+      //Send JSON of posts collection
+      post.find({}, function(err, doc){
+         if (err) throw err;
+          res.json(doc);
       });
-      /*
-              var newupdate = new updatelist({
-                update_time: new Date(),
-                posts: singlepost.id
-              });
-          */
+
+    }
+    else {
+      console.log("error");
+    }
+
+    //Deletes previous collection
+    post.remove({}, function(err, doc){
+       if (err) throw err;
     });
-  }
-  else {
-    console.log("error");
-  }
+  
+  });
+
 });
+
 
 //Return router
 module.exports = router;
